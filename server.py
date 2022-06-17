8d2b1d233f7dd21c258fd023b1fc9e71ed8f096c
tmax818 <tylermaxwell661@gmail.com>
2022-06-17 13:48:15 -0700

    finish?

---
+++ server.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+from thing import Thing
+
+# ! CREATE
+
+@app.route('/new')
+def new():
+    return render_template('new.html')
+
+@app.route('/create', methods=['POST'])
+def create():
+    print(request.form)
+    Thing.save(request.form)
+    return redirect('/')
+
+# ! READ / RETRIEVE
+
+@app.route('/')
+def index():
+    return render_template('index.html', things = Thing.get_all())
+
+@app.route('/show/<int:id>')
+def show(id):
+    data = {'id': id}
+    return render_template('show.html', thing = Thing.get_one(data))
+
+# ! UPDATE
+
+@app.route('/edit/<int:id>')
+def edit(id):
+    data = {'id': id}
+    return render_template('edit.html', thing = Thing.get_one(data) )
+
+@app.route('/update', methods=['post'])
+def update():
+    print(request.form)
+    Thing.update(request.form)
+    return redirect('/')
+
+# ! DELETE
+
+@app.route('/destroy/<int:id>')
+def destroy(id):
+    data = {'id': id}
+    Thing.destroy(data)
+    return redirect('/')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ thing.py
@@ -0,0 +1,41 @@
+from mysqlconnection import connectToMySQL
+
+DATABASE = 'one_thing'
+
+class Thing:
+    def __init__( self , data ):
+        self.id = data['id']
+        self.property = data['property']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+
+    @classmethod
+    def get_all(cls):
+        query = "SELECT * FROM things;"
+        results = connectToMySQL(DATABASE).query_db(query)
+        things = []
+        for thing in results:
+            things.append( Thing(thing) )
+        return things
+
+    @classmethod
+    def get_one(cls, data):
+        query = "SELECT * FROM things WHERE id = %(id)s;"
+        result = connectToMySQL(DATABASE).query_db(query, data)
+        return Thing(result[0])
+        
+    @classmethod
+    def save(cls, data):
+        query = "INSERT INTO things (property) VALUES (%(property)s)"
+        return connectToMySQL(DATABASE).query_db(query, data)
+
+    @classmethod
+    def update(cls, data):
+        query = "UPDATE things SET property=%(property)s WHERE id = %(id)s"
+        return connectToMySQL(DATABASE).query_db(query, data)
+
+    @classmethod
+    def destroy(cls, data):
+        query = "DELETE FROM things WHERE id = %(id)s;"
+        return connectToMySQL(DATABASE).query_db(query, data)
+        
